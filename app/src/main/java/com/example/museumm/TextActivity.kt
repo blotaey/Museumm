d8b2108044f799bdae7d84be716d5d40842cebbd
Lucas Ferreira <lucasskidss@gmail.com>
2020-10-13 17:50:41 -0300

    Posição atual funcionando

--- app/src/main/java/com/example/museumm/TextActivity.kt
+++ app/src/main/java/com/example/museumm/TextActivity.kt
@@ -1,11 +1,18 @@
 package com.example.museumm
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_text.*
 
 class TextActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text)
+
+        btn_submit.setOnClickListener{
+            val intent = Intent(applicationContext, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
